162de6ae09d370bd8942576aa285a1b9a3f0073b
ToyJoyYo <alibek_adilzhanov@mail.ru>
2020-11-14 17:55:28 +0600

    removed these files

--- env_django_1/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\alibe\desktop\sabaq\codes\git_luck\python_advanced\django_with_girls\env_django_1\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
